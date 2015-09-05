c17759db1d14a80147c6d34fc880f4a7529bd62c
Brad Aylsworth <Brad.Aylsworth@gmail.com>
2015-09-05 08:53:59 -0500

    Info init works

--- windows.py
+++ windows.py
@@ -10,6 +10,10 @@ def quote_path(path):
     return '"' + str(path.absolute()) + '"'
 
 class Info:
+    """Holds relevant info from mkvinfo"""
+
+    titleregex = re.compile(r'\| \+ Title: (\S*)')
+
     def pprint(self):
         """Pretty print this Info object"""
         print(self.title)
@@ -30,57 +34,48 @@ class Info:
         args = [quote_path(x) for x in args]
         result = check_output(' '.join(args)).decode()
 
-        self.tracks = []
-        buf = result
-        while True:
-            pos = buf.find('\n')
-            if pos == -1:
-                break
-            line = buf[:pos].strip()
-            buf = buf[pos+1:].strip()
+        match = Info.titleregex.search(result)
+        self.title = match.group(1).strip()
 
-            if line.startswith('| + Title:'):
-                match = re.search(r': (.+)$', line)
-                self.title = match.group(1).strip()
-            elif line.startswith('|+ EbmlVoid'):
-                # We've passed all useful information
-                break
-            elif line.startswith('| + A track'):
-                pos = buf.find('| + A track')
-                if pos == -1:
-                    self.tracks.append( self.Track(buf) )
-                    break
-                else:
-                    self.tracks.append( self.Track(buf[:pos]) )
-                    buf = buf[pos+1:]
+        # Split into tracks and trim excess
+        sections = result.split("| + A track")[1:]
+        sections[-1] = sections[-1].split("|+")[0]
+        self.tracks = []
+        for group in sections:
+            self.tracks.append(self.Track(group.strip()))
+
 
     class Track:
         def pprint(self):
-            print("{type} track {name}:".format(self))
-            print("  Codec:    {codec}".format(self))
-            print("  Language: {language}".format(self))
-            print("  Track ID: {id}".format(self))
+            print("{0.type} -- {0.name}:".format(self))
+            print("  Codec:    {0.codec}".format(self))
+            print("  Language: {0.language}".format(self))
+            print("  Track ID: {0.id}".format(self))
 
         def __init__(self, buf):
+            self.name = 'undefined'
+            self.language = 'undefined'
+            # Other fields shouldn't be blank
             while True:
                 """arguments: `buf` as string from mkvinfo output"""
                 pos = buf.find('\n')
                 if pos == -1:
                     break
                 line = buf[:pos].strip()
-                buf = buf[pos+1].strip()
+                buf = buf[pos+1:].strip()
+                
 
                 if "Track number:" in line:
                     match = re.search(r': ([\d]+)\)\s*$', line)
                     self.id = int(match.group(1))
                 elif "Track type:" in line:
-                    match = re.search(r': (\w+)$', line)
+                    match = re.search(r': (\S+)$', line)
                     self.type = match.group(1)
                 elif "Codec ID:" in line:
-                    match = re.search(r': (\w+)$', line)
+                    match = re.search(r': (\S+)$', line)
                     self.codec = match.group(1)
                 elif "Language:" in line:
-                    match = re.search(r': (\w+)$', line)
+                    match = re.search(r': (\S+)$', line)
                     self.language = match.group(1)
                 elif "Name:" in line:
                     match = re.search(r': (.*)$', line)
